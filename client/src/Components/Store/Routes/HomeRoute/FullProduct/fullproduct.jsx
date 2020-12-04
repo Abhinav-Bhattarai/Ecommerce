e0c6019fb9e40831f80eeffd9f08e1d2e6a59541
Abhinav Bhattarai <messibhattarai3@gmail.com>
2020-12-04 18:30:51 +0545

    FS: more changes

--- client/src/Components/Store/Routes/HomeRoute/FullProduct/fullproduct.jsx
+++ client/src/Components/Store/Routes/HomeRoute/FullProduct/fullproduct.jsx
@@ -1,8 +1,35 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-
+import { IconContext } from 'react-icons';
+import { FaAngleLeft, FaAngleRight, FaUserCircle } from 'react-icons/fa';
 import './fullproduct.scss';
 import { withRouter } from 'react-router';
+
+const UserIcon = ()=>{
+    return (
+        <IconContext.Provider value={{
+            className: 'user-icon'
+        }}>
+            <FaUserCircle/>
+        </IconContext.Provider>
+    )
+}
+
+const AngleRight = ()=>{
+    return (
+        <IconContext.Provider value={{className: 'angular-icon'}}>
+            <FaAngleRight/>
+        </IconContext.Provider>
+    )
+}
+
+const AngleLeft = ()=>{
+    return (
+        <IconContext.Provider value={{className: 'angular-icon'}}>
+            <FaAngleLeft/>
+        </IconContext.Provider>
+    )
+}
 
 const FullProduct = (props) => {
 
@@ -14,8 +41,9 @@ const FullProduct = (props) => {
             axios.get(`/alternate-product-search/${props.match.params.id}`).then((response)=>
             {
                 // invalidity
-                if(response.data === {invalid_id: true}){
+                if(JSON.stringify(response.data) === JSON.stringify({invalid_id: true})){
                     SetInvalidity(true)
+                    props.history.push('/main-product')
                 }else{
                     Setdata(response.data)
                 }
@@ -27,9 +55,30 @@ const FullProduct = (props) => {
     let product_jsx = null
     if(data){
         product_jsx = (
-            <div className='full-product-image-container'>
-                <img src={data.ProductImage} alt='product-desc'/>
-            </div>    
+            <main>
+                <div className='full-product-image-container'>
+                    <img src={data.ProductImage} alt='product-desc'/>
+                    <div className='angular-icon-container'><AngleLeft/></div>
+                    <div className='angular-icon-container-2'><AngleRight/></div>
+                </div>    
+                <div className='full-product-ItemName'>{data.ItemName}</div>
+                <div className='full-product-Description'>{data.Description}</div>
+                <div className='full-product-seller-name'>
+                   <div className='full-product-seller-name-tag'>Seller: </div>
+                   <div className='full-product-seller-name-icon'><UserIcon/></div>
+                   <div className='full-product-seller-name-fullname'>{data.Seller}</div>
+                </div>
+                <main className='full-product-button-container'>
+                    <div className='full-product-price'>
+                        <header className='tag'>PURCHASE NOW</header>
+                        <footer className='full-product-price-tag'>Price: Rs {data.Price}</footer>
+                    </div>
+                    <div className='full-product-price full-product-wishlist'>
+                        <header className='tag'>CHECKOUT</header>
+                        <footer className='full-product-price-tag'>Price: Rs {data.Price}</footer>
+                    </div>
+                </main>
+            </main>
         )
     }
 
--- server/Routes/alternate-product-search.js
+++ server/Routes/alternate-product-search.js
@@ -7,6 +7,8 @@ router.get('/:id', (req, res)=>{
     const id = req.params.id
     ProductModel.findById(id).then((response)=>{
         return res.json(response)
+    }).catch(()=>{
+        return res.json({invalid_id: true})
     })
 })
 
