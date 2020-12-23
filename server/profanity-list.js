e7a61de3cb7e9db3262078e74a5ffe308fe5a408
Abhinav Bhattarai <messibhattarai3@gmail.com>
2020-12-23 12:20:26 +0545

    version: 1.0.0 alpha

--- server/profanity-list.js
+++ server/profanity-list.js
@@ -1,3 +1,5 @@
 // hey don't look at this adult content These are just for profanity check, vulgarity, violence and racial unnecessary comments in the review
 
 export const profanity_list = ['porn', 'fuck', 'shit', 'tits', 'boobs', 'dick', 'pussy', 'vagina', 'penis', 'condom', 'bitch', 'fucker', 'sucker', 'gay', 'sexy', 'ass', 'nigga', 'mf', 'fcuk', 'strip', 'whore', 'hoe', 'simp', 'chink', 'racist', 'profanity', 'vulgarity', 'erection', 'poop', 'dung', 'wtf']
+
+// Going to add microsoft azure profanity detection algorithm in near future
